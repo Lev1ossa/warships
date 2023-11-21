ec91b88e699bffeb9c35db9b1f6c0df14eddc352
Lev1ossa <ikehhui@gmail.com>
2023-11-21 16:59:54 +0300

    feat: add loader & fix styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss';
 export const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>Warships</h1>
+      <h1 className={styles.title}>Warships</h1>
     </header>
   );
 };
--- src/components/WarshipsList/WarshipsList.tsx
+++ src/components/WarshipsList/WarshipsList.tsx
@@ -1,4 +1,5 @@
 import { useVehicles } from '../../hooks/useVehicles';
+import { Loader } from '../Loader/Loader';
 import { Warship } from '../Warship/Warship';
 import styles from './WarshipsList.module.scss';
 
@@ -7,24 +8,27 @@ export function WarshipsList() {
 
   console.log(data);
 
-  if (loading) return 'Loading...';
-  if (error) return <pre>{error.message}</pre>;
-
   return (
     <main className={styles.main}>
-      <div className={styles.warshipsContainer}>
-        <div className={styles.warshipsList}>
-          {data ? (
-            <>
-              {data.vehicles.map((vehicle, idx) => (
-                <Warship key={`${vehicle.title}${idx}`} vehicle={vehicle} />
-              ))}
-            </>
-          ) : (
-            <p>Error</p>
-          )}
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className={styles.warshipsContainer}>
+          <div className={styles.warshipsList}>
+            {data ? (
+              <>
+                {data.vehicles.map((vehicle, idx) => (
+                  <Warship key={`${vehicle.title}${idx}`} vehicle={vehicle} />
+                ))}
+              </>
+            ) : error ? (
+              <p>{error.message}</p>
+            ) : (
+              <p>Error!</p>
+            )}
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
